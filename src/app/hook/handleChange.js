59fa19887070b77c2469cb6184d826fc9b4931bf
NehalAhmedQureshi <153610346+NehalAhmedQureshi@users.noreply.github.com>
2025-02-12 17:20:52 +0500

    add console.log for debug

--- src/app/hook/handleChange.js
+++ src/app/hook/handleChange.js
@@ -1,4 +1,5 @@
 export const handleChange = (event,setTextField) => {
     const { name, value } = event.target;
+    console.log(name , 'name')
     setTextField((prev) => ({ ...prev, [name]: value }));
   };
